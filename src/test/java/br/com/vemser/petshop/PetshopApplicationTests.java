916930d71f0d04dd7b8c66c6079c621a87e87a3b
Gabriel Luppi <luppi.gabriel08@gmail.com>
2022-07-14 17:56:15 -0300

    Adicionando projeto Spring

---
+++ src/test/java/br/com/vemser/petshop/PetshopApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.vemser.petshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetshopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
