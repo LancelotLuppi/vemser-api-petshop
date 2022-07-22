5f12bf5ff86d1eb048d3aaa32e8f667b04c64850
Gabriel Luppi <luppi.gabriel08@gmail.com>
2022-07-22 13:49:52 -0300

    Removendo ConexaoBancoDeDados

--- src/main/java/br/com/vemser/petshop/config/ConexaoBancoDeDados.java
+++ src/main/java/br/com/vemser/petshop/config/ConexaoBancoDeDados.java
@@ -1,35 +1,35 @@
-package br.com.vemser.petshop.config;
-
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Bean;
-import org.springframework.stereotype.Component;
-import org.springframework.web.context.annotation.RequestScope;
-
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
-
-@Component
-public class ConexaoBancoDeDados {
-
-    @Value("${jdbc-string}")
-    private String jdbcString;
-
-    @Value("${jdbc-user}")
-    private String user;
-
-    @Value("${jdbc-pass}")
-    private String pass;
-
-    @Value("${jdbc-schema}")
-    private String schema;
-
-
-    public Connection getConnection() throws SQLException{
-        Connection con = DriverManager.getConnection(jdbcString, user, pass);
-
-        con.createStatement().execute("alter session set current_schema=" + schema);
-
-        return con;
-    }
-}
+//package br.com.vemser.petshop.config;
+//
+//import org.springframework.beans.factory.annotation.Value;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.stereotype.Component;
+//import org.springframework.web.context.annotation.RequestScope;
+//
+//import java.sql.Connection;
+//import java.sql.DriverManager;
+//import java.sql.SQLException;
+//
+//@Component
+//public class ConexaoBancoDeDados {
+//
+//    @Value("${jdbc-string}")
+//    private String jdbcString;
+//
+//    @Value("${jdbc-user}")
+//    private String user;
+//
+//    @Value("${jdbc-pass}")
+//    private String pass;
+//
+//    @Value("${jdbc-schema}")
+//    private String schema;
+//
+//
+//    public Connection getConnection() throws SQLException{
+//        Connection con = DriverManager.getConnection(jdbcString, user, pass);
+//
+//        con.createStatement().execute("alter session set current_schema=" + schema);
+//
+//        return con;
+//    }
+//}
