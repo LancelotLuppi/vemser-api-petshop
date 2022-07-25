54f9129de7acc382a16cf172acddd0eb1cd72b76
Gabriel Luppi <luppi.gabriel08@gmail.com>
2022-07-25 09:24:21 -0300

    Removendo imports desnecessarios repository

--- src/main/java/br/com/vemser/petshop/repository/ContatoRepository.java
+++ src/main/java/br/com/vemser/petshop/repository/ContatoRepository.java
@@ -2,8 +2,6 @@ package br.com.vemser.petshop.repository;
 
 import br.com.vemser.petshop.entity.ContatoEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
