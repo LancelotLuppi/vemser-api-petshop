8998eb05291df64c25b23713d8449b40dea83706
Mateus de Castro <ttukstro@gmail.com>
2022-07-29 15:26:59 -0300

    adicionando o cors

--- src/main/java/br/com/vemser/petshop/security/SecurityConfiguration.java
+++ src/main/java/br/com/vemser/petshop/security/SecurityConfiguration.java
@@ -7,9 +7,12 @@ import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.config.annotation.web.configurers.AuthorizeHttpRequestsConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 @EnableWebSecurity
@@ -46,5 +49,26 @@ public class SecurityConfiguration {
                         .antMatchers(HttpMethod.DELETE, "/contato").hasAnyRole("ADMIN", "USER", "ATENDENTE")
                         .antMatchers(HttpMethod.DELETE, "/pedido").hasRole("ADMIN")
                         .anyRequest().authenticated();
+    }
+
+
+    @Bean
+    public WebSecurityCustomizer webSecurityCustomizer(){
+        return (web) -> web.ignoring().antMatchers("/v3/api-docs",
+                "/v3/api-docs/**",
+                "/swagger-resources/**",
+                "/swagger-ui/**");
+    }
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer(){
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry){
+                registry.addMapping("/**")
+                        .allowedMethods("*")
+                        .exposedHeaders("Authorization");
+            }
+        };
     }
 }
