736af00cc798a526659360700d7beb671c60f339
Gabriel Luppi <luppi.gabriel08@gmail.com>
2022-07-29 15:20:44 -0300

    Adicionando permissoes iniciais da aplicacao

--- src/main/java/br/com/vemser/petshop/security/SecurityConfiguration.java
+++ src/main/java/br/com/vemser/petshop/security/SecurityConfiguration.java
@@ -3,8 +3,11 @@ package br.com.vemser.petshop.security;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AuthorizeHttpRequestsConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
@@ -20,11 +23,28 @@ public class SecurityConfiguration {
         https.headers().frameOptions().disable().and()
                 .cors().and()
                 .csrf().disable()
-                .authorizeHttpRequests((authz) ->
-                        authz.antMatchers("/cliente/**").hasRole("ADMIN")
-                                .anyRequest().authenticated()
-                );
+                .authorizeHttpRequests(permissoes());
         https.addFilterBefore(new TokenAuthenticationFilter(tokenService), UsernamePasswordAuthenticationFilter.class);
         return https.build();
+    }
+
+    private Customizer<AuthorizeHttpRequestsConfigurer<HttpSecurity>.AuthorizationManagerRequestMatcherRegistry> permissoes() {
+        return (authz) ->
+                authz.antMatchers(HttpMethod.POST, "/cliente").hasAnyRole("USER", "ADMIN")
+                        .antMatchers(HttpMethod.POST, "/pet").hasAnyRole("USER", "ADMIN")
+                        .antMatchers(HttpMethod.POST, "/contato").hasAnyRole("USER", "ADMIN")
+                        .antMatchers(HttpMethod.POST, "/pedido").hasAnyRole("USER", "ATENDENTE", "TOSADOR")
+                        .antMatchers(HttpMethod.GET, "/cliente").hasAnyRole("ADMIN", "ATENDENTE")
+                        .antMatchers(HttpMethod.GET, "/pet").hasAnyRole("ADMIN", "TOSADOR")
+                        .antMatchers(HttpMethod.GET, "/contato").hasAnyRole("ADMIN", "ATENDENTE")
+                        .antMatchers(HttpMethod.PUT, "/cliente").hasAnyRole("USER", "ADMIN")
+                        .antMatchers(HttpMethod.PUT, "/pet").hasAnyRole("USER", "ADMIN")
+                        .antMatchers(HttpMethod.PUT, "/contato").hasAnyRole("USER", "ADMIN", "ATENDENTE")
+                        .antMatchers(HttpMethod.PUT, "/pedido").hasAnyRole("USER", "ADMIN", "ATENDENTE")
+                        .antMatchers(HttpMethod.DELETE, "/cliente").hasRole("ADMIN")
+                        .antMatchers(HttpMethod.DELETE, "/pet").hasAnyRole("ADMIN", "USER")
+                        .antMatchers(HttpMethod.DELETE, "/contato").hasAnyRole("ADMIN", "USER", "ATENDENTE")
+                        .antMatchers(HttpMethod.DELETE, "/pedido").hasRole("ADMIN")
+                        .anyRequest().authenticated();
     }
 }
