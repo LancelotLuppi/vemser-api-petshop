e31b80d727795a64184117e110b72044a9d06f82
Gabriel Luppi <luppi.gabriel08@gmail.com>
2022-07-29 19:09:01 -0300

    Adicionando update de cargos

--- src/main/java/br/com/vemser/petshop/service/CargosService.java
+++ src/main/java/br/com/vemser/petshop/service/CargosService.java
@@ -1,25 +1,54 @@
 package br.com.vemser.petshop.service;
 
 import br.com.vemser.petshop.entity.CargoEntity;
-import br.com.vemser.petshop.entity.ClienteEntity;
 import br.com.vemser.petshop.entity.UsuarioEntity;
 import br.com.vemser.petshop.enums.TipoCargo;
 import br.com.vemser.petshop.exception.EntidadeNaoEncontradaException;
 import br.com.vemser.petshop.exception.RegraDeNegocioException;
+import br.com.vemser.petshop.repository.CargoRepository;
+import br.com.vemser.petshop.repository.UsuarioRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
 
+import java.util.Set;
+import java.util.function.Predicate;
+
+@Service
+@RequiredArgsConstructor
 public class CargosService {
 
+    private final UsuarioService usuarioService;
+    private final CargoRepository cargoRepository;
+    private final UsuarioRepository usuarioRepository;
 
-    private UsuarioService usuarioService;
+    public void updateCargo(Integer idUsuario, TipoCargo tipoCargo) throws RegraDeNegocioException, EntidadeNaoEncontradaException {
+        UsuarioEntity user = usuarioService.findById(idUsuario);
+        CargoEntity cargo = returnByEnum(tipoCargo);
 
-    public void updateCargo(UsuarioEntity usuario, TipoCargo cargo) throws RegraDeNegocioException, EntidadeNaoEncontradaException {
-        if(usuario.getCargos().equals(cargo)){
-            throw new RegraDeNegocioException("O User já está com esse cargo!");
+        if (user.getCargos().stream().anyMatch((Predicate<? super CargoEntity>) cargo)) {
+            throw new RegraDeNegocioException("O usuario já tem esse cargo!");
         }
-        if(usuario.getCargos().equals(TipoCargo.USUARIO)){
 
+        user.setCargos(Set.of(cargo));
+        usuarioRepository.save(user);
+    }
+
+    public CargoEntity returnByEnum(TipoCargo tipoCargo) throws EntidadeNaoEncontradaException {
+        if(tipoCargo.equals(TipoCargo.ADMIN)) {
+            return findById(1);
+        } else if(tipoCargo.equals(TipoCargo.ATENDENTE)) {
+            return findById(4);
+        } else if(tipoCargo.equals(TipoCargo.TOSADOR)) {
+            return findById(3);
+        } else if(tipoCargo.equals(TipoCargo.USUARIO)) {
+            return findById(2);
         }
+        return null;
+    }
 
+    public CargoEntity findById(Integer idCargo) throws EntidadeNaoEncontradaException {
+        return cargoRepository.findById(idCargo)
+                .orElseThrow(() -> new EntidadeNaoEncontradaException("Cargo não encontrado"));
     }
 
 
