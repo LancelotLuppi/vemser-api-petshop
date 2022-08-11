d83fa8d343b48eafda172870ef59671c766ebccf
Mateus de Castro <ttukstro@gmail.com>
2022-08-11 16:29:26 -0300

    scheduled feito para email marketing

--- src/main/java/br/com/vemser/petshop/repository/UsuarioRepository.java
+++ src/main/java/br/com/vemser/petshop/repository/UsuarioRepository.java
@@ -1,13 +1,17 @@
 package br.com.vemser.petshop.repository;
 
+import br.com.vemser.petshop.entity.CargoEntity;
 import br.com.vemser.petshop.entity.UsuarioEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface UsuarioRepository extends JpaRepository<UsuarioEntity, Integer> {
 
     Optional<UsuarioEntity> findByUsername(String username);
+
+
 }
